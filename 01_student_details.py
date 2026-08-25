f53e6dd3485a45bcdc085d38965f296937de200b
Sohini Ball <ballsohini939@gmail.com>
2026-08-25 23:19:44 +0530

    Add student details program

---
+++ 01_student_details.py
@@ -0,0 +1,13 @@
+import sys
+
+name = input("Enter your name: ")
+roll_number = input("Enter your roll number: ")
+course_name = input("Enter your course name: ")
+semester = input("Enter your semester: ")
+
+print("\n--- Student Details ---")
+print("Name:", name)
+print("Roll Number:", roll_number)
+print("Course Name:", course_name)
+print("Semester:", semester)
+print("Python Version:", sys.version)
